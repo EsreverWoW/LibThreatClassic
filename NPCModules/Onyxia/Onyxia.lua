386ad29068cf5a189adb07099c4df0111b58f631
PFStein <pfstein91@gmail.com>
2019-12-02 15:24:44 -0600

    Initial implementation of blizzard hotfix to fireball threat

---
+++ NPCModules/Onyxia/Onyxia.lua
@@ -0,0 +1,20 @@
+local ThreatLib = LibStub and LibStub("ThreatClassic-1.0", true)
+if not ThreatLib then return end
+
+local ONYXIA_ID = 6109
+local ONYXIA_FIREBALL_SPELL = 18392
+
+
+ThreatLib:GetModule("NPCCore"):RegisterModule(
+    ONYXIA_ID, 
+    function(Onyxia)
+        function Onyxia:Init()
+            self:RegisterCombatant(ONYXIA_FIREBALL_SPELL, true)
+            self:RegisterSpellHandler("SPELL_DAMAGE", self.Fireball, ONYXIA_FIREBALL_SPELL, ONYXIA_FIREBALL_SPELL)
+        end
+
+        function Onyxia:Fireball()
+            self:WipeRaidThreatOnMob(ONYXIA_ID)
+        end
+    end
+)
